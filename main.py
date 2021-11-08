aaeace6aa1934832e97d5eaeb2487f2c54077aaf
dradler-pbx <dominik.radler@productbloks.com>
2021-11-08 10:27:26 +0100

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import os
 
-filepath = 'data'
+filepath = 'data\HCB'
+parameterID = pd.read_excel('ParameterID.xlsx')
+
 
 data = pd.DataFrame()
-for ()
+files = os.listdir(filepath)
+
+for file in files:
+    df = pd.read_csv(filepath+'\\'+file, sep=';')
+    data.merge(df, on='title')
+
+new_header = []
+for id in data.columns:
+    id_clear = parameterID[parameterID['ID(hex)']==id]['Custom name']
+    new_header.append(id_clear)
+
+data.columns = new_header
